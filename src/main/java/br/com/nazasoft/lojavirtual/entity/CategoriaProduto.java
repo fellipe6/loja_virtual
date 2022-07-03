7057f9ab58ccd646d0cae0892936dcf64b3c8c12
fellipe6 <webmaster.crio@gmail.com>
2022-07-03 19:06:28 -0300

    Criação das entidades categoria_produto e marca_produto

---
+++ src/main/java/br/com/nazasoft/lojavirtual/entity/CategoriaProduto.java
@@ -0,0 +1,25 @@
+package br.com.nazasoft.lojavirtual.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto",sequenceName = "seq_categoria_produto",allocationSize = 1,initialValue = 1)
+public class CategoriaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_categoria_produto")
+	private Long id;
+	
+	@Column(name="nome_desc",nullable = false)
+	private String nomeDesc;
+}
