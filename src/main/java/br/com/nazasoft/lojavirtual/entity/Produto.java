39e7f9689e05857be17c49dadd850130c6efcc26
fellipe6 <webmaster.crio@gmail.com>
2022-07-07 19:53:19 -0300

    Criação das entidades e associaçoes: CupDes,ImagemProduto,NotaFiscaCompra, NotaItemProduto e produto.

---
+++ src/main/java/br/com/nazasoft/lojavirtual/entity/Produto.java
@@ -0,0 +1,182 @@
+package br.com.nazasoft.lojavirtual.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+@SequenceGenerator(name = "seq_produto", sequenceName = "seq_produto", allocationSize = 1, initialValue = 1)
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
+	private Long id;
+
+	private String tipoUnidade;
+
+	private String nome;
+
+	private Boolean ativo = Boolean.TRUE;
+
+	@Column(columnDefinition = "text", length = 2000)
+	private String descricao;
+
+	private Double peso;
+
+	private Double largura;
+
+	private Double altura;
+
+	private BigDecimal valorVenda = BigDecimal.ZERO;
+
+	private Integer QTDEstoque = 0;
+
+	private Integer QTDAlertaEstoque = 0;
+
+	private String linkYoutube;
+
+	private Boolean alerta_QTDEstoque = Boolean.FALSE;
+
+	private Integer qtdClique = 0;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipoUnidade() {
+		return tipoUnidade;
+	}
+
+	public void setTipoUnidade(String tipoUnidade) {
+		this.tipoUnidade = tipoUnidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Double getLargura() {
+		return largura;
+	}
+
+	public void setLargura(Double largura) {
+		this.largura = largura;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public BigDecimal getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(BigDecimal valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public Integer getQTDEstoque() {
+		return QTDEstoque;
+	}
+
+	public void setQTDEstoque(Integer qTDEstoque) {
+		QTDEstoque = qTDEstoque;
+	}
+
+	public Integer getQTDAlertaEstoque() {
+		return QTDAlertaEstoque;
+	}
+
+	public void setQTDAlertaEstoque(Integer qTDAlertaEstoque) {
+		QTDAlertaEstoque = qTDAlertaEstoque;
+	}
+
+	public String getLinkYoutube() {
+		return linkYoutube;
+	}
+
+	public void setLinkYoutube(String linkYoutube) {
+		this.linkYoutube = linkYoutube;
+	}
+
+	public Boolean getAlerta_QTDEstoque() {
+		return alerta_QTDEstoque;
+	}
+
+	public void setAlerta_QTDEstoque(Boolean alerta_QTDEstoque) {
+		this.alerta_QTDEstoque = alerta_QTDEstoque;
+	}
+
+	public Integer getQtdClique() {
+		return qtdClique;
+	}
+
+	public void setQtdClique(Integer qtdClique) {
+		this.qtdClique = qtdClique;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
